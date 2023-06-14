152c82d9d7cf3ff15a6eba3658b077d566a2206a
Zach <zachkoh0871@gmail.com>
2023-06-14 14:23:03 +0800

    finished the logic with javascript
    made an API call to generate the random quotes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-class quote {
-    
+const link = "https://type.fit/api/quotes";
+
+const generateButton = document.querySelector('[data-generate-button]')
+const quote = document.querySelector('[data-quote-output]')
+
+generateButton.addEventListener("click",fetchQuote);
+
+function fetchQuote(){
+    fetch(link)
+    .then(response => response.json())
+    .then(data => {
+      const randomQuoteIndex = Math.round(Math.random()*data.length);
+      quote.innerHTML = data[randomQuoteIndex].text;
+      (data[randomQuoteIndex]);
+    });
 }
+fetchQuote();
